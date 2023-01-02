5cb443d88d7df48e4c80d50686d229925a8b86fe
JustAbusiness <huysanti123456@gmail.com>
2023-01-02 23:01:35 +0700

    Start on

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    let todos = ["Huy", "Robert", "Pham"];
+
+</script>
+
+<ol>
+
+    {#each todos as item}
+        <li> {item}</li>
+        <li> {item}</li>
+        <li> {item}</li>
+    {/each}
+
+</ol>
